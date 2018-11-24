6e0c06c0785359c0ae70591cada5220ee5937fe8
adover134 <adfsfsf@naver.com>
2018-11-24 11:42:40 +0900

    Ctrl + [ TestCase

--- GureumTests/GureumTests.swift
+++ GureumTests/GureumTests.swift
@@ -195,4 +195,25 @@ class GureumTests: XCTestCase {
             XCTAssertEqual("\"", app.client.string, "buffer: \(app.client.string) app: \(app)")
         }
     }
+
+    func testEscapeOrCntrlAndLeftBracketHan3Gureum() {
+        for app in self.apps {
+            app.client.string = ""
+            app.controller.setValue(GureumInputSourceIdentifier.han3FinalNoShift.rawValue, forTag: kTextServiceInputModePropertyTag, client: app.client)
+            
+            app.inputText("[", key: UInt(kVK_ANSI_LeftBracket), modifiers: NSEvent.ModifierFlags.control)
+            XCTAssertEqual("", app.client.string, "buffer: \(app.client.string) app: \(app)")
+        }
+    }
+    
+    func testEscapeOrCntrlAndLeftBracketWithShiftHan3Gureum() {
+        for app in self.apps {
+            let controlAndShift = NSEvent.ModifierFlags.control.union(.shift)
+            app.client.string = ""
+            app.controller.setValue(GureumInputSourceIdentifier.han3FinalNoShift.rawValue, forTag: kTextServiceInputModePropertyTag, client: app.client)
+            
+            app.inputText("[", key: UInt(kVK_ANSI_LeftBracket), modifiers: controlAndShift)
+            XCTAssertEqual("", app.client.string, "buffer: \(app.client.string) app: \(app)")
+        }
+    }
 }
